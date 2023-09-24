7c5378f1df7b706a4ffae5755dff59a95a3287a4
Constance Amatey <consamate57@gmail.com>
2023-09-24 23:32:35 +0000

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+
+/**
+ * lomuto_partition - creates order of element based on lomuto partition scheme
+ * @array: array of integers
+ * @size: size of the array
+ * @left: The start index of the first set of element
+ * @right: The end index of the second set of element
+ * Return: The final partition index
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_ele(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap_ele(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+
+/**
+ * lomuto_sort - sorts elements based on lomuto scheme
+ * @array: array of integers
+ * @size: size of the array
+ * @left: The start index of the first set of element
+ * @right: The end index of the second set of element
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+
+
+/**
+ * quick_sort - Sort an array of integers using quick sort
+ * @array: array of integers
+ * @size: size of the array
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+    int part;
+
+	if (!array || size < 2)
+		return;
+
+
+	lomuto_sort(array, size, 0, size - 1);
+}
--- sort.h
+++ sort.h
@@ -27,6 +27,9 @@ void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right)
+void quick_sort(int *array, size_t size);
 
 
 #endif
