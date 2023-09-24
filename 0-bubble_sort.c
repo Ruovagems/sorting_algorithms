36674a629265b5e4574c852198f032cfbfb32494
Constance Amatey <consamate57@gmail.com>
2023-09-24 03:21:44 +0000

    worked on errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,13 +34,13 @@ void bubble_sort(int *array, size_t size)
 	while (swapped == false)
 	{
 		swapped = true;
-		for (i = 0, i < len - 1, i++)
+		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i] + 1)
 			{
 				swap_ele(array + i, array + i + 1);
 				print_array(array, size);
-				sorted = false;
+				swapped = false;
 			}
 		}
 		len--;
