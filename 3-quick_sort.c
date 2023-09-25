a586b7596004c639053503fa92e37901c4eeb5d3
Constance Amatey <consamate57@gmail.com>
2023-09-25 01:02:29 +0000

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -80,7 +80,7 @@ void lomuto_sort(int *array, size_t size, int left, int right)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	lomuto_sort(array, size, 0, size - 1);
